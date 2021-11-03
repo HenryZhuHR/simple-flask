4be71ae637290a933df269e7fba6e07e7b4e2306
Henry Zhu <38650110+HenryZhuHR@users.noreply.github.com>
2021-11-03 09:55:31 +0800

    new

--- server.py
+++ server.py
@@ -5,19 +5,23 @@ app = Flask(__name__)
 app.debug = True
 
 
+ERROR_POST_DATA_INVALID = 10
+ERROR_IMAGE_INVALID = 11
+
+
+
 @app.route('/', methods=['post'])
 def add_stu():
-    if not request.data:  # 检测是否有数据
-        return ('fail')
+    if not request.data:  # check if valid data
+        return (ERROR_POST_DATA_INVALID)
+
     data_get = request.data.decode('utf-8')
 
     data_json = json.loads(data_get)  # 把区获取到的数据转为JSON格式
     print(data_json['command'])
-    # print(data_json['image'])
 
-    return jsonify(data_json)  # 返回JSON数据。
+    return 10
 
 
 if __name__ == '__main__':
     app.run(host='192.168.1.141', port=2021)
-    # 这里指定了地址和端口号。
